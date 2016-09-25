76f1c29172501b9b077d465261a4ee6d5cea186b
Bogdan Lungu <lungu.c.bogdan@gmail.com>
2016-09-25 21:56:34 +0300

    Names, scopes and a class example

---
+++ names-scopes-classes.py
@@ -0,0 +1,39 @@
+#names declared in the local scope
+person = 'john'
+age = 12
+
+def show_info():
+    person2 = 'smith' #name declared in the enclosing scope
+
+print(person)
+
+#name is not accessible - declared in function enclosing scope 
+#print(person2)
+
+def local():
+    m = 7
+    print(m)
+
+m = 5
+print(m)
+
+local()
+
+
+#define a class
+class Bike:
+    def __init__(self, colour, frame_material): #sort of constructor in JavaScript
+        self.colour = colour
+        self.frame_material = frame_material
+
+
+    def brake(self): #declare a new method on the class
+        print("Braking!")
+
+#instantiate the class
+cube = Bike('Red', 'Aluminium')
+pegas = Bike('Black', 'Steel')
+
+print(cube.colour)
+print(pegas.frame_material)
+print(cube.brake())

9ae15c8557cb0df4e97c0568cfb9cb9987d5384b
Bogdan Lungu <lungu.c.bogdan@gmail.com>
2016-09-26 22:31:15 +0300

    Draw a square with turtle

--- take-a-break.py
+++ take-a-break.py
@@ -1,11 +1,12 @@
 import time
 import webbrowser
 
-total_breaks = 3
+total_breaks = 16
 break_count = 0
 
 print("This program started on "+time.ctime())
 while (break_count < total_breaks):
-    time.sleep(60*30)
+    time.sleep(60*35)
+    print("Breake started at "+time.ctime())
     webbrowser.open("https://www.youtube.com/watch?v=MrHxhQPOO2c")
     break_count = break_count + 1

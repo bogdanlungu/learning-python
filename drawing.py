4088b08e54dc880ead9ceaaf048cf20aefc9aafc
Bogdan Lungu <lungu.c.bogdan@gmail.com>
2016-09-27 22:43:16 +0300

    Draw a circle out of rectangles

--- drawing.py
+++ drawing.py
@@ -1,21 +1,33 @@
 import turtle
 
-def draw_square():
+def draw_square(some_turtle):
+    for i in range(1,5):
+        some_turtle.forward(100)
+        some_turtle.right(90)
+
+def draw_art():
     window = turtle.Screen()
-    window.bgcolor("red")
-    
+    window.bgcolor("gray")
+
+    #Draw the square
     brad = turtle.Turtle()
     brad.shape("turtle")
     brad.color("yellow")
-    brad.forward(100)
-    brad.right(90)
-    brad.forward(100)
-    brad.right(90)
-    brad.forward(100)
-    brad.right(90)
-    brad.forward(100)
-    brad.right(90)
+    brad.speed(2)
+
+    for i in range(1,37):
+        draw_square(brad)
+        brad.right(10)
+    
+    #Draw a circle
+    #Instantiate turtle again
+    ''''
+    angie = turtle.Turtle()
+    angie.shape("arrow")
+    angie.color("blue")
+    angie.circle(100)
+    '''
 
     window.exitonclick()
 
-draw_square()
+draw_art()

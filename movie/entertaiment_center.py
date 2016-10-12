75272cadcebbb820809821fa2a8eff01d643630d
Bogdan Lungu <lungu.c.bogdan@gmail.com>
2016-10-12 23:13:42 +0300

    Experimenting with classes

---
+++ movie/entertaiment_center.py
@@ -0,0 +1,7 @@
+import media
+
+toy_story = media.Movie("Toy Story", "Some description of the movie here",
+                        "http://upload.wikipedia.org/wikipedia/en/1/13/Toy_Story.jpg",
+                        "https://www.youtube.com/watch?v=KYz2wyBy3kc")
+#print(toy_story.storyline)
+toy_story.show_trailer()

9e2969c0fb441da177f395154e5fa588e54ed8a9
Bogdan Lungu <lungu.c.bogdan@gmail.com>
2016-11-03 21:36:08 +0200

    Some OOP

---
+++ oop.py
@@ -0,0 +1,32 @@
+class Fleet:
+
+    def __init__(self):
+        self._cars = []
+
+    def add_car(self, car):
+        self._cars.append(car)
+
+    def remove_car(self, car):
+        self,_people.remove(car)
+
+    def show_fleet(self):
+        for car in self._cars:
+            car.display_brand()
+
+
+class Car:
+
+    def __init__(self, brand):
+        self.brand = brand
+
+    def display_brand(self):
+        print(id(self))
+        print("The brand is: ", self.brand)
+
+
+fleet = Fleet()
+fleet.add_car(Car("Audi"))
+fleet.add_car(Car("BMW"))
+fleet.add_car(Car("Chevrolet"))
+fleet.add_car(Car("Fiat"))
+fleet.show_fleet()

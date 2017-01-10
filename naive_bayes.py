35a51a4b41a43264f166478084b7313c36766e9e
Bogdan Lungu <lungu.c.bogdan@gmail.com>
2017-01-10 22:50:30 +0200

    Naive Bayes example

--- naive_bayes.py
+++ naive_bayes.py
@@ -1,2 +1,12 @@
 """ Naive Bayes - Gaussian Naive Bayes """
 # pylint: disable=C0103
+
+from sklearn.naive_bayes import GaussianNB
+import numpy as np
+
+A = np.array([[-1, -1], [-2, -1], [-3, -2], [1, 1], [2, 1], [3, 2]])
+B = np.array([1, 1, 1, 2, 2, 2])
+clf = GaussianNB() #create classifier
+
+clf.fit(A, B)
+print(clf.predict([[1, 1]]))

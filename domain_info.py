2ecbc0159eb8621022162ab73528431f6c457b9f
Bogdan Lungu <lungu.c.bogdan@gmail.com>
2017-02-15 23:17:23 +0200

    Verify domain name

--- domain_info.py
+++ domain_info.py
@@ -1,6 +1,7 @@
 """ Get info about a domain
 """
 # pylint: disable=C0103
+import re
 import whois
 
 
@@ -12,9 +13,21 @@ def get_domain_name():
 
 def process_the_request(domain):
     """Process the request"""
-    result = whois.whois(domain)
-    print(result)
+    if check_domain(domain):
+        result = whois.whois(domain)
+        print(result)
+    else:
+        print('The domain name is not valid.')
 
+
+def check_domain(domain):
+    """Check if the domain is valid"""
+    if len(domain) > 255:
+        return False
+    if domain[-1] == ".":
+        domain = domain[:-1]
+    allowed = re.compile("(?!-)[A-Z\d-]{1,63}(?<!-)$", re.IGNORECASE)
+    return all(allowed.match(x) for x in domain.split("."))
 
 the_domain = get_domain_name()
 process_the_request(the_domain)

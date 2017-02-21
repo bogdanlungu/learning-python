f79542d2a881dfc1c49b3e299856e30328dc5b1b
Bogdan Lungu <lungu.c.bogdan@gmail.com>
2017-02-21 23:51:39 +0200

    Added functions to md5.py

--- md5.py
+++ md5.py
@@ -3,11 +3,17 @@
 import hashlib
 
 
+def get_input():
+    """Get input"""
+    print("Welcome! \nEnter the string to you want to convert:")
+    return input()
+
+
 def toMD5(string):
     """To md5"""
     m = hashlib.md5()
     m.update(string.encode('utf-8'))
-    return m.hexdigest()
+    print("The hash is: \n" + m.hexdigest())
 
-m = toMD5('hello')
-print(m)
+the_input = get_input()
+toMD5(the_input)
